d433fad19a5883a8ca081c22166fdfde4eb4ce0f
takorejsza <takorejs@umich.edu>
2022-10-11 08:57:44 -0400

    updated db with pxl-per-micron info

--- get-morphology.py
+++ get-morphology.py
@@ -1,10 +1,24 @@
 import os
 import numpy as np
 import pandas as pd
+import sqlite3
+import re
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 from skimage.io import imread
 from skimage.measure import label, regionprops
+
+
+def get_pixel_to_area_conversion(im: str, db:str=r"MetalDAM/MetalDAM_metadata.sql") -> float:
+
+    with sqlite3.connect(db) as conn:
+        cur = conn.cursor()
+
+        sql = f"""SELECT pixels_per_micron FROM micrograph WHERE image_path LIKE '%{im}';"""
+        cur.execute(sql)
+        conversion = cur.fetchone()
+        conversion = float(conversion[0])
+    return conversion
 
 if __name__=="__main__":
     # Once DBSCAN is implemented, create function to get feature properties.
@@ -20,6 +34,8 @@ if __name__=="__main__":
 
         im_og = imread(f"MetalDAM/cropped_grayscale/{pic}", as_gray=True)
         im_lb = imread(f"MetalDAM/labels/{pic.replace('jpg','png')}", as_gray=True)
+
+        conversion = get_pixel_to_area_conversion(pic)
         
         for k in np.unique(im_lb):
             #  mask = np.select([im_lb==k, im_lb!=k], [True, False], im_og)
@@ -27,13 +43,13 @@ if __name__=="__main__":
             labels = label(mask, connectivity=2)
 
             # Uncomment to see phase comparision.
-            fig, ax = plt.subplots(1,2)
-            ax[0].set_title(f"labeled {PHASE_MAP[k]}")
-            ax[0].imshow(mask)
-            ax[1].set_title(f"Raw {PHASE_MAP[k]}")
-            ax[1].imshow(labels)
-            # plt.title(f"{pic}\n count: {count} , phase {k}")
-            plt.show()
+            # fig, ax = plt.subplots(1,2)
+            # ax[0].set_title(f"labeled {PHASE_MAP[k]}")
+            # ax[0].imshow(mask)
+            # ax[1].set_title(f"Raw {PHASE_MAP[k]}")
+            # ax[1].imshow(labels)
+            # # plt.title(f"{pic}\n count: {count} , phase {k}")
+            # plt.show()
 
             props = ["area", "area_filled", "axis_major_length", "axis_minor_length",
                     "eccentricity", "equivalent_diameter_area", "feret_diameter_max",
@@ -46,6 +62,7 @@ if __name__=="__main__":
             {l: {p: properties[l][p] for p in props} for l in range(1, labels.max())}, orient='index'
             )
             try:
+                # change to test if results.empty then del results.
                 results["phase_number"] =  k
                 results["phase"] = PHASE_MAP[k]
                 results["picture"] = pic
@@ -54,6 +71,10 @@ if __name__=="__main__":
             except:
                 del results
                 continue
+
+            area_cols = [c for c in results.columns if re.search(r"area|diameter|length",c)]
+            for col in area_cols:
+                results[col] = results[col] / conversion
 
             morphological_data.append(results)
 
