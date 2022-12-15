b91c82c36cbc46ae67bf5ea83c2c93c421cb47e1
Nicholas David <59184306+nrdavid@users.noreply.github.com>
2022-12-15 16:29:23 -0500

    nick final push

--- unsoup.py
+++ unsoup.py
@@ -20,7 +20,8 @@ from collections import defaultdict
 import seaborn as sns
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import MinMaxScaler, PowerTransformer
+from sklearn.preprocessing import MinMaxScaler, PowerTransformer, StandardScaler
+from os.path import exists
 
 PROPS = _regionprops.PROPS
 PROP_DTYPES = _regionprops.COL_DTYPES
@@ -31,7 +32,7 @@ for x in PROPS.values():
 
 def felzenszwalb_img(img):
     img = img_as_float(img)
-    segments_fz = felzenszwalb(img, scale=100, sigma=0.5, min_size=50)
+    segments_fz = felzenszwalb(img, scale=100, sigma=0.5, min_size=500)
     return segments_fz
 
 def reduce_img(img, segments):
@@ -57,7 +58,31 @@ def map_reduced_labels_to_og(labels, segments, df):
     return seg_copy
 
 def get_morphological_data(img, segments):
-    props = regionprops_table(segments, intensity_image=img, properties=numerical_props)
+    PROPS = [
+        "area",
+        "area_bbox",
+        "area_filled",
+        "area_convex",
+        "axis_major_length",
+        "axis_minor_length",
+        "eccentricity",
+        "equivalent_diameter_area",
+        "euler_number",
+        "extent",
+        "feret_diameter_max",
+        "intensity_max",
+        "intensity_mean",
+        "intensity_min",
+        "orientation",
+        "perimeter",
+        "perimeter_crofton",
+        "solidity"
+    ]
+    props = regionprops_table(segments, intensity_image=img, properties=PROPS)
+    # print("segs")
+    # print(segments)
+    # print("img")
+    # print(img)
     df = pd.DataFrame.from_dict(props)
     return df
 
@@ -79,13 +104,15 @@ def do_kmeans(im_og, pic_name):
     kneedle = KneeLocator(distorsions, range(2, 8), S=1.0, curve='convex', direction='decreasing')
     plt.plot(kneedle.x_normalized, kneedle.y_normalized, label="Distortions")
     plt.plot(kneedle.x_difference, kneedle.y_difference, label='Difference')
+    # plt.plot(kneedle.x, kneedle.y, label="Distortions")
+    # plt.plot(kneedle.x, kneedle.y, label='Difference')
     plt.vlines(kneedle.norm_elbow, 0, 1, linestyles='--', label=f"S = 1.0, K = {round(kneedle.elbow_y, 2)}")
     plt.xlabel('Distortions')
     plt.ylabel('K')
     plt.legend()
     plt.grid(True)
     plt.title('Elbow curve')
-    plt.savefig(f'{pic_name}_elbow_curve.png')
+    plt.savefig(f'nick_all_pics/{pic_name}_elbow_curve.png')
     im_show_labels = np.copy(im_og)
     count =  0
     for (i, j), v in np.ndenumerate(im_og):
@@ -95,7 +122,7 @@ def do_kmeans(im_og, pic_name):
     plt.imshow(im_show_labels)
     plt.title(f"Kmeans Generated Labels with K = {kneedle.elbow_y}.")
     plt.colorbar()
-    plt.savefig(f'{pic_name}_kmeans_labels.png')
+    plt.savefig(f'nick_all_pics/{pic_name}_kmeans_labels.png')
     plt.close()
     return im_show_labels
 
@@ -143,10 +170,10 @@ def plot_quad(img_og, img_new, img_label, hand_labeled, fname, filter_method='No
     fig, axs = plt.subplots(2, 2, figsize=(10, 10))
     fig.tight_layout()
     # top left
-    axs[0, 0].imshow(img_og, cmap='viridis')
+    axs[0, 0].imshow(img_og, cmap='Greys')
     axs[0, 0].set_title("Original Microstructure")
     # top right
-    axs[0, 1].imshow(img_new, cmap='viridis')
+    axs[0, 1].imshow(img_new, cmap='Greys')
     axs[0, 1].set_title(f"Augmented Image by {filter_method}")
     # bottom left
     axs[1, 0].imshow(img_label, cmap='viridis')
@@ -157,7 +184,7 @@ def plot_quad(img_og, img_new, img_label, hand_labeled, fname, filter_method='No
     plt.savefig(fname)
     plt.close()
 
-def gen_fez_segment(pic1, kmeans):
+def gen_fez_segment(pic1, kmeans, fname):
     img = imread(f"MetalDAM/cropped_grayscale/{pic1}", as_gray=True)
     segmented_fz_og = felzenszwalb_img(img)
     segmented_fz_kmeans = felzenszwalb_img(kmeans)
@@ -177,17 +204,17 @@ def gen_fez_segment(pic1, kmeans):
     # bottom right
     axs[1, 1].imshow(fz_segs_kmeans, cmap='viridis')
     axs[1, 1].set_title(f"Segnmented by Felzenszwalb Method after KMeans")
-    plt.savefig("felzenszwalb.png")
+    plt.savefig(fname)
     plt.close()
     return segmented_fz_og
 
 
-def gen_gaussian_blur_quad(pic1, pic2):
+def gen_gaussian_blur_quad(pic1, pic2, fname):
     img = imread(f"MetalDAM/cropped_grayscale/{pic1}", as_gray=True)
     img_new = skimage.filters.gaussian(img)
     img_hand_labeled = imread(f"MetalDAM/labels/{pic2}")
     img_labeled = do_kmeans(img_new, pic1)
-    fname = "gaussian_blur_quad.png"
+    #fname = "gaussian_blur_quad.png"
     plot_quad(img, img_new, img_labeled, img_hand_labeled, fname, filter_method='Gaussian Blur', cluster_method="KMeans")
     return img_labeled
 
@@ -205,6 +232,7 @@ def assign_islands_labels(fz_seg, img_labeled):
 def gen_props(fz_seg, img, img_labeled):
     fz_seg += 1
     df_props = get_morphological_data(img, fz_seg)
+    df_props_og = df_props.copy(deep=True)
     labs = []
     ail = assign_islands_labels(fz_seg, img_labeled)
     for i, row in enumerate(df_props.iterrows()):
@@ -212,13 +240,13 @@ def gen_props(fz_seg, img, img_labeled):
         labs.append(ail[j])
     df_props['label_majority'] = labs
     df_props = df_props.dropna(axis=1)
-    return df_props
+    return df_props, df_props_og
 
-def do_tsne(df_props):
+def do_tsne(df_props, fname):
     np_props = df_props.to_numpy()
-    tsne = TSNE(n_components=2, verbose=1, init='random', perplexity=30)
+    tsne = TSNE(n_components=2, verbose=1, init='random', perplexity=20)
     dat = np_props[:, 0:-1]
-    scaler = MinMaxScaler()
+    scaler = StandardScaler()
     #scaler = PowerTransformer()
     scaled_dat = scaler.fit_transform(dat)
     z = tsne.fit_transform(scaled_dat)
@@ -231,7 +259,7 @@ def do_tsne(df_props):
                 palette=sns.color_palette("hls", max(df.y.tolist())+1),
                 data=df)
     sns_plot.set(title="Morphology Data Projection")
-    sns_plot.figure.savefig("tsne_plot.png")
+    sns_plot.figure.savefig(fname)
 
 def do_pca_kmeans(df_props):
     np_props = df_props.to_numpy()
@@ -245,14 +273,16 @@ def do_pca_kmeans(df_props):
     
 
 def main():
+    df_tsne = pd.DataFrame()
+    df_props_og_all = pd.DataFrame()
     for pic1, pic2 in zip(sorted(os.listdir(r"MetalDAM/cropped_grayscale")), sorted(os.listdir(r"MetalDAM/labels"))):
         img = imread(f"MetalDAM/cropped_grayscale/{pic1}", as_gray=True)
-        img_labeled = gen_gaussian_blur_quad(pic1, pic2)
-        fz_seg = gen_fez_segment(pic1, img_labeled)
-        df_props = gen_props(fz_seg, img, img_labeled)
-        do_tsne(df_props)
-        #do_pca_kmeans(df_props)
-        break
+        img_labeled = gen_gaussian_blur_quad(pic1, pic2, f"nick_all_pics/gaussian_blur_quad_{pic1}")
+        fz_seg = gen_fez_segment(pic1, img_labeled, f"nick_all_pics/felzenszwalb_{pic1}")
+        df_props, df_props_og = gen_props(fz_seg, img, img_labeled)
+        df_props_og['picture'] = pic1
+        df_props_og_all = pd.concat([df_props_og_all, df_props_og])
+    df_props_og_all.to_pickle("tom_final.pkl")
     pass
 
 if __name__ == "__main__":
